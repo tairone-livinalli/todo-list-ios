d0b01c0160d97b6df5e25a9e25c0112728c6c2fb
Tairone Livinalli <taironelivinalli@hotmail.com>
2024-11-04 14:02:13 -0500

    Initial Commit

---
+++ todo-list/todo_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  todo_listApp.swift
+//  todo-list
+//
+//  Created by Tairone Livinalli on 2024-11-04.
+//
+
+import SwiftUI
+
+@main
+struct todo_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ todo-listTests/todo_listTests.swift
@@ -0,0 +1,17 @@
+//
+//  todo_listTests.swift
+//  todo-listTests
+//
+//  Created by Tairone Livinalli on 2024-11-04.
+//
+
+import Testing
+@testable import todo_list
+
+struct todo_listTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
